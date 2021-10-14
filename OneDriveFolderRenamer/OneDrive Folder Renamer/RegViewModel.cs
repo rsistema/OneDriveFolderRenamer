8c01ec553d9ec8faab648a8585dde93b556ee334
Rodrigo Rodrigues <rodrigo.rodrigues@gamasti.com.br>
2021-10-13 22:30:54 -0300

    initial commit

---
+++ OneDriveFolderRenamer/OneDrive Folder Renamer/RegViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDrive_Folder_Rename
+{
+    public class RegViewModel
+    {
+        public string Key { get; set; }
+        public string Path { get; set; }
+        public string Name { get; set; } = "";
+        public string Value { get; set; }
+    }
+}
